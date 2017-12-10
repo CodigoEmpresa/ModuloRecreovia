c680d8659a60fb3013d96652a879fe50c7134e56
Jonathan Castro <jonatancastro1@gmail.com>
2017-12-10 15:11:18 -0500

    Cambio producto no conforme a sesiones finalizadas. pendiente realizar pruebas

--- app/Http/Controllers/Recreovia/ReporteProductoNoConformeController.php
+++ app/Http/Controllers/Recreovia/ReporteProductoNoConformeController.php
@@ -7,6 +7,7 @@ use App\Modulos\Recreovia\GrupoPoblacional;
 use App\Modulos\Recreovia\Jornada;
 use App\Http\Controllers\Controller;
 use App\Modulos\Recreovia\Reporte;
+use App\Modulos\Recreovia\Sesion;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
@@ -20,10 +21,7 @@ class ReporteProductoNoConformeController extends Controller
             $sesiones = null;
         } else {
 
-            $qb = Reporte::with(['sesiones' => function($query)
-            {
-                $query->with('productoNoConforme', 'profesor.persona', 'gestorSiAsume.persona');
-            }]);
+            $qb = Sesion::with('productoNoConforme', 'profesor.persona', 'gestorSiAsume.persona');
 
             $qb = $this->aplicarFiltros($qb, $request);
 
@@ -34,35 +32,25 @@ class ReporteProductoNoConformeController extends Controller
 
             $sesiones = collect([]);
 
-            foreach ($elementos as $reporte)
-            {
-                foreach ($reporte->sesiones as $sesion)
-                {
-                    $exists = $sesiones->search(function($item, $key) use ($sesion)
-                    {
-                        return $item->Id == $sesion->Id;
-                    }, true);
+            foreach ($elementos as $sesion) {
+                $exists = $sesiones->search(function ($item, $key) use ($sesion) {
+                    return $item->Id == $sesion->Id;
+                }, true);
 
-                    if (is_bool($exists))
-                    {
-                        if ($request->has('no_conformidad'))
-                        {
-                            $pass = false;
-                            foreach ($request->input('no_conformidad') as $no_conformidad)
-                            {
-                                if($sesion->productoNoConforme[$no_conformidad] == '0')
-                                {
-                                    $pass = true;
-                                }
+                if (is_bool($exists)) {
+                    if ($request->has('no_conformidad')) {
+                        $pass = false;
+                        foreach ($request->input('no_conformidad') as $no_conformidad) {
+                            if ($sesion->productoNoConforme[$no_conformidad] == '0') {
+                                $pass = true;
                             }
-
-                            if($pass) {
-                                $sesiones->push($sesion);
-                            }
-                        } else {
-                            $sesiones->push($sesion);
                         }
 
+                        if ($pass) {
+                            $sesiones->push($sesion);
+                        }
+                    } else {
+                        $sesiones->push($sesion);
                     }
                 }
             }
@@ -91,34 +79,32 @@ class ReporteProductoNoConformeController extends Controller
 
         if($request->has('id_localidad'))
         {
-            $qb->whereHas('punto', function($query) use ($request) {
+            $qb->whereHas('cronograma.punto', function($query) use ($request) {
                 $query->whereIn('Id_Localidad', $request->input('id_localidad'));
             });
         }
 
         if($request->has('id_upz'))
         {
-            $qb->whereHas('punto', function($query) use ($request) {
+            $qb->whereHas('cronograma.punto', function($query) use ($request) {
                 $query->whereIn('Id_Upz', $request->input('id_upz'));
             });
         }
 
         if($request->has('id_punto'))
         {
-            $qb->whereHas('punto', function($query) use ($request) {
+            $qb->whereHas('cronograma.punto', function($query) use ($request) {
                 $query->whereIn('Id_Punto', $request->input('id_punto'));
             });
         }
 
         if($request->input('fecha_inicio') || $request->input('fecha_fin'))
         {
-            $qb->whereHas('sesiones', function($query) use ($request) {
-                if ($request->input('fecha_inicio'))
-                    $query->where('Fecha', '>=', $request->input('fecha_inicio'));
+            if ($request->input('fecha_inicio'))
+                $qb->where('Fecha', '>=', $request->input('fecha_inicio'));
 
-                if ($request->input('fecha_fin'))
-                    $query->where('Fecha', '<=', $request->input('fecha_fin'));
-            });
+            if ($request->input('fecha_fin'))
+                $qb->where('Fecha', '<=', $request->input('fecha_fin'));
         }
 
         return $qb;
